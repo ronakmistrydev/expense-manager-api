e6307fb3b659077975111b88fdb8a08f2d2ca60e
Ronak Mistry <ronakmistry36@gmail.com>
2022-04-15 02:34:03 -0400

    First commit

---
+++ src/test/java/com/main/expensemanagerapi/ExpenseManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.expensemanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseManagerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
