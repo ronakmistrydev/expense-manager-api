0ec4bb05766804cc03cd3fd73265e36ef2873abf
Ronak Mistry <ronakmistry36@gmail.com>
2022-05-10 12:59:08 -0500

    Imports fixed

--- src/main/java/com/main/expensemanagerapi/service/MutationService.java
+++ src/main/java/com/main/expensemanagerapi/service/MutationService.java
@@ -20,18 +20,17 @@ import java.util.*;
 
 @Service
 public class MutationService {
-    private final OrganizationEntityRepository organizationEntityRepository;
     private final AccountEntityRepository accountEntityRepository;
-    private final CategoryRepository categoryRepository;
-
     private final AccountRecordEntityRepository accountRecordEntityRepository;
+    private final CategoryRepository categoryRepository;
+    private final OrganizationEntityRepository organizationEntityRepository;
 
     @Autowired
     public MutationService(
-        OrganizationEntityRepository organizationEntityRepository,
         AccountEntityRepository accountEntityRepository,
+        AccountRecordEntityRepository accountRecordEntityRepository,
         CategoryRepository categoryRepository,
-        AccountRecordEntityRepository accountRecordEntityRepository
+        OrganizationEntityRepository organizationEntityRepository
     ) {
         this.organizationEntityRepository = organizationEntityRepository;
         this.accountEntityRepository = accountEntityRepository;
@@ -73,17 +72,12 @@ public class MutationService {
     }
 
     public List<Account> findAccounts(String organizationId, String userSub) {
-        // todo:: check if organization belongs to this user
         String storedOrganizationId = this.organizationEntityRepository.getByUserSub(userSub).getId();
         if (!Objects.equals(storedOrganizationId, organizationId)) throw new InvalidRequestStateException("Organization does not belongs to this user");
         return this.accountEntityRepository.findByOrganizationId(organizationId);
     }
 
     public String createAccount(String userSub, CreateAccount createAccount) {
-        // todo:: check if organization belongs to this user
-        // todo:: check if duplicate - (name, type)
-        // todo:: get balance from payload and create initial record under this account
-        // todo:: account type specific logic should belongs to sub-class for each type of account (SavingsAccount, CheckIn account etc..)
         Account account = new Account(
                 UUID.randomUUID().toString(),
                 createAccount.getOrganizationId(),
@@ -101,13 +95,6 @@ public class MutationService {
     }
 
     public String createRecord(String userSub, CreateAccountRecord createAccountRecord) {
-        // todo:: check if to account belongs to given organization
-        // todo:: check if given user belongs to an organization
-        // todo :: check organization account and user have a relation or connection
-
-        // todo:: fix serialization issue for date
-        // todo:: fix created at logic
-
         AccountRecord accountRecord = new AccountRecord(
                 UUID.randomUUID().toString(),
                 createAccountRecord.getCategoryId(),
@@ -125,15 +112,10 @@ public class MutationService {
     }
 
     public List<AccountRecord> findRecords(String organizationId, String userSub) {
-        // todo:: check if account belongs to given organization
         return this.accountRecordEntityRepository.findByOrganization(organizationId);
     }
 
     public String updateRecord(String userSub, UpdateAccountRecord updateAccountRecord) {
-        // todo:: check if account record exists
-        // todo:: check if account belongs to given organization
-        // todo:: check if user belongs to given organization
-
         AccountRecord accountRecord = new AccountRecord(
                 UUID.randomUUID().toString(),
                 updateAccountRecord.getCategoryId(),
@@ -149,19 +131,4 @@ public class MutationService {
         this.accountRecordEntityRepository.save(accountRecord);
         return accountRecord.getId();
     }
-
-
-    /*
-     * 4. different versions of account creation
-     * 10. add payee
-     * 11. update payee
-     * 12. get all payee
-     * 13. add friends
-     * 14. add portion with friends on a record
-     *
-     *
-     * Nice to have
-     * 1. update account (name and type)
-     * 2. validations
-     */
 }
